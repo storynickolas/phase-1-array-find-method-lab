6c53d68c37de467883958e9f146d01448959e1d7
Nick Story <storynickolasl@gmail.com>
2022-05-05 08:41:01 -0400

    commit tree edc32e9989dbc48a18cc074c5be28d3052e87acd

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function superbowlWin(franchiseHistory) {
+  let win = []
+  franchiseHistory.find(function(game) {
+    if(game.result === 'W'){
+      win.push(game.year)
+    }
+  })
+  return win[0]
+}
+
+
+
+
+
